15b867500e2e47988ce6571ea46be85a9e2c0bdb
Arthur Ming <guobin.ming@pwc.com>
2023-06-20 13:41:30 +0800

    feat: add hi-there anchor

--- components/title/index.tsx
+++ components/title/index.tsx
@@ -3,7 +3,9 @@ import * as React from "react";
 export const Title: React.FC = () => {
   return (
     <h1 className="pt-1 text-[2em]">
-      <strong>Hi there 👋</strong>
+      <a href="#hi-there">
+        <strong id="hi-there">Hi there 👋</strong>
+      </a>
     </h1>
   );
 };

2bd8a65a9661432d726a5b5b2bc213b7698ab1ec
Arthur Ming <mingguobin@live.com>
2023-06-20 12:36:46 +0800

    fix: set px-4 for mobile view

--- components/language-icon/index.tsx
+++ components/language-icon/index.tsx
@@ -9,7 +9,5 @@ interface LanguageIconProps {
 export const LanguageIcon: React.FC<LanguageIconProps> = ({ name, alt }) => {
   const url = `assets/${name}.svg`;
 
-  console.log(url);
-
   return <Image src={url} alt={alt} width={40} height={40} />;
 };

eaf1ebb2b30490a4ca1f505bb97607752b33a5ea
Arthur Ming <guobin.ming@pwc.com>
2023-07-14 09:00:49 +0800

    fix: add divider for language and tool title

--- components/languages-and-tools/index.tsx
+++ components/languages-and-tools/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { LanguageIcon } from "@/components/language-icon";
+import { Divider } from "../divier";
 
 interface LanguageAndToolsProps {
   languages: Array<Record<"name" | "alt", string>>;
@@ -15,6 +16,7 @@ export const LanguageAndTools: React.FC<LanguageAndToolsProps> = ({
           <h2 id="languages-and-tools">🌈 Languages and Tools:</h2>
         </a>
       </div>
+      <Divider className="mt-2" />
       <div className="mt-4 flex flex-wrap">
         {languages.map(({ name, alt }) => (
           <LanguageIcon key={name} name={name} alt={alt} />

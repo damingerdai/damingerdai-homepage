f90f1bfee1680ff3344d452f968c8b3eedd77ae0
damingerdai <mingguobin@live.com>
2024-10-30 21:08:30 +0800

    feat: remove PwC link

--- components/about-me/index.tsx
+++ components/about-me/index.tsx
@@ -4,16 +4,7 @@ export const AboutMe: React.FC = () => {
   return (
     <ul role="list" className="text-sm list-inside list-disc pl-5 space-y-3">
       <li>🖥 I&acute;m a full-stack developer based in Shanghai, China.</li>
-      <li>
-        💼 I was working at
-        <a
-          className="text-blue-500 pl-1"
-          href="https://www.linkedin.com/company/pwc-ac-shanghai"
-        >
-          PwC Acceleration Center Shanghai
-        </a>
-        .
-      </li>
+      <li>💼 I am looking for a job .</li>
       <li>
         ✍️ I wrote dev articles on
         <a className="text-blue-500 pl-1" href="https://damingerdai.github.io">

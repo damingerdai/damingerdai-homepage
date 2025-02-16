c6a5a851241ddfaa5484b0cdc16012f2e3e599df
damingerdai <mingguobin@live.com>
2025-02-16 22:07:23 +0800

    feat: add roate in navbar title

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -4,7 +4,7 @@ import { ThemeToggleButton } from "../theme-toggle-button";
 export const Navbar = () => {
   return (
     <nav className="sticky px-4 h-16 w-full z-10 flex items-center shadow-md">
-      <h1 className="font-bold">大明二代</h1>
+      <h1 className="font-bold hover:rotate-6">大明二代</h1>
       <div className="flex-1"></div>
       <ThemeToggleButton />
     </nav>

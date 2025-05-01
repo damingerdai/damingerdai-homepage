ed8236a7a432a0582a28a0faef06c65886b24130
damingerdai <mingguobin@live.com>
2025-05-01 16:20:32 +0800

    fix: fix the github stats hydration error

--- components/my-github-stats/img.tsx
+++ components/my-github-stats/img.tsx
@@ -1,25 +1,27 @@
-'use client';
+"use client";
 /* eslint-disable @next/next/no-img-element */
-import * as React from 'react';
+import * as React from "react";
 import { useTheme } from "next-themes";
 
 export const MyGithubStatsImg = () => {
-    const {resolvedTheme} = useTheme();
-    const url = React.useMemo(() => {
-        if (resolvedTheme === "dark") {
-            return "https://github-readme-stats.damingerdai.com/api?username=damingerdai&theme=dark";
-        } else if (resolvedTheme === 'light') {
-            return "https://github-readme-stats.damingerdai.com/api?username=damingerdai&theme=default";
-        } else {
-            return "https://github-readme-stats.damingerdai.com/api?username=damingerdai"
-        }
-    }, [resolvedTheme]);
+  const { resolvedTheme } = useTheme();
+  const url = React.useMemo(() => {
+    if (resolvedTheme === "dark") {
+      return "https://github-readme-stats.damingerdai.com/api?username=damingerdai&theme=dark";
+    } else if (resolvedTheme === "light") {
+      return "https://github-readme-stats.damingerdai.com/api?username=damingerdai&theme=default";
+    } else {
+      return "https://github-readme-stats.damingerdai.com/api?username=damingerdai&theme=default";
+    }
+  }, [resolvedTheme]);
 
-    return <img
-        className="w-full"
-        width={500}
-        height={220}
-        src={url}
-        alt="damingerdai's github stat"
+  return (
+    <img
+      className="w-full"
+      width={500}
+      height={220}
+      src={url}
+      alt="damingerdai's github stat"
     />
-}
+  );
+};
